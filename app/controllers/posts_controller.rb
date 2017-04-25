a80730f9a4f5ff2d95f5d47d8aed6c4b1fc8a5c6
Kieraj Mumick <kmumick@seas.upenn.edu>
2017-04-25 13:54:43 -0400

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create post_params
+    if @post.save
+      redirect_to :action => :index
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id]
+    @post.update post_params
+    if @post.save
+      redirect_to post_path @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find params[:id]
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ActiveRecord::Base
+	validates :image, presence: true
+
+	# Let Paperclip know that this requires an attached file, and resize that image to the width provided.
+	# The resizing is handled by ImageMagick.
+	has_attached_file :image, styles: {
+		size:  "600x"
+	}
+	do_not_validate_attachment_file_type :image
+end
