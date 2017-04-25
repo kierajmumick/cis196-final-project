9c84aa33081e7c53a3a95d977c9959abd1cd0abd
Kieraj Mumick <kmumick@seas.upenn.edu>
2017-04-25 18:08:39 -0400

    likes model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true
