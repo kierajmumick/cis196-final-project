01ff731491de310d3b572eed4992858efd4d2cb5
Kieraj Mumick <kmumick@seas.upenn.edu>
2017-04-26 01:38:40 -0400

    justin kims approval

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,21 @@
 class CommentsController < ApplicationController
 
+	def create
+		@post = Post.find params[:post_id]
+		@comment = Comment.create comment_params
+		@comment.user = current_user
+		@post.comments.append @comment
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			redirect_to posts_path
+		end
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
+
 end
